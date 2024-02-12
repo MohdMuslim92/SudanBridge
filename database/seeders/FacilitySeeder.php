491d8d63c8ebae448449b4a14a438209df6fe6cc
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-12 23:28:33 +0300

    Add database seeder for Facilities data

---
+++ database/seeders/FacilitySeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Facility;
+class FacilitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $facilities = [
+            [
+                'name' => 'Main store',
+                'location' => 'Portsodan',
+            ],
+            [
+                'name' => 'Distribution center',
+                'location' => 'Portsodan',
+            ],
+            [
+                'name' => 'Main store',
+                'location' => 'Atbra',
+            ],
+            [
+                'name' => 'Distribution center',
+                'location' => 'Atbra',
+            ],
+            [
+                'name' => 'Main store',
+                'location' => 'Khartoum',
+            ],
+            [
+                'name' => 'Distribution center',
+                'location' => 'Khartoum',
+            ],
+            [
+                'name' => 'Distribution center',
+                'location' => 'Bahri',
+            ],
+            [
+                'name' => 'Distribution center',
+                'location' => 'Khartoum',
+            ],
+        ];
+
+        Facility::insert($facilities);
+    }
+}
