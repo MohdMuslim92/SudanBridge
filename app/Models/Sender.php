1f35752bce914a0206b6ee33650eecb0a5d9bb48
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-14 02:44:20 +0300

    Add Sender model
    Created the Sender model to represent the senders data.

---
+++ app/Models/Sender.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sender extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address_id',
+    ];
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
+
+}
