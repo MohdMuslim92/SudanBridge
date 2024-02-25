58f20725ee94d814dae0fae82ddf6f321ef212b8
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-25 15:54:20 +0300

    Enhanced the login page by adding a 'New user?' link
    - The link directing users to the registration page.
    - This improves user experience by providing a clear
      pathway for new users to sign up while maintaining
      consistency with the existing design and functionality.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -78,6 +78,13 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <Link
+                    :href="route('register')"
+                    class="mr-4 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    New user?
+                </Link>
+
+                <Link
                     v-if="canResetPassword"
                     :href="route('password.request')"
                     class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
