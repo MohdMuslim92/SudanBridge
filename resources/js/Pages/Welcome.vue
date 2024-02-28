9a1b7a12eb0a5a9caca5dbbb56bf29d78ef4bdd2
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-28 03:23:58 +0300

    Add link to track packages on landing page
    Added a link to the landing page that directs users
    to the tracking page, allowing them to easily track
    their packages. The "Track Package" link is styled
    to match the existing design and provides a clear
    call-to-action for users.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -83,8 +83,13 @@ startTimer();
 
             <template v-else>
                 <Link
-                    :href="route('login')"
+                    :href="route('tracking')"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Track Package</Link>
+
+                <Link
+                    :href="route('login')"
+                    class="pl-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                 >Contact Us</Link>
 
                 <Link
