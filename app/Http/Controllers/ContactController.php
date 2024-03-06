157757837df4973ef2a892cd781c44461ff6bf79
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-03-06 16:17:44 +0300

    Feat: Add ContactController for handling Contact Us page
    This commit introduces the ContactController, responsible
    for rendering the Contact Us page using Inertia.js. The
    controller contains a single method, index(), which
    returns the Inertia render of the 'Contact' page.
    This controller facilitates the routing and handling
    of requests related to the Contact Us page within the application.

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ContactController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Inertia::render('Contact');
+    }
+
+}
