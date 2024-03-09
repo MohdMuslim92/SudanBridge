b46d37e464a67396544a6bdb73d37c878f3a9f81
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-03-10 02:08:41 +0300

    Add sender-shipment-out-for-delivery blade template
    Introduce a new blade template,
    sender-shipment-out-for-delivery.blade.php,
    responsible for rendering the email content to notify
    the sender that their shipment is out for delivery.
    The template includes dynamic content such as the
    sender's name, recipient's name, recipient's address,
    shipment item name, and tracking token. It also
    provides a link for tracking the shipment and includes
    social media icons with links to the company's social
    media profiles.

---
+++ resources/views/emails/sender-shipment-out-for-delivery.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shipment Out For Delivery</title>
+</head>
+<body>
+<!-- Header -->
+<div style="background-color: #f8f9fa; padding: 20px; text-align: center;">
+    <img src="http://127.0.0.1:8000/img/sudanbridge.png" alt="{{ config('app.name') }} Logo"  style="max-width: 200px;">
+</div>
+
+<!-- Body -->
+<div style="padding: 20px;">
+    <h1 style="text-align: center;">Shipment Out for Delivery</h1>
+    <p>Dear <strong>{{ $shipment->sender->name }}</strong>,</p>
+    <p>Your shipment <strong>{{ $shipment->item->name }}</strong> is now out for delivery to {{ $shipment->recipient->name }}'s address.</p>
+    <p>Here are the details:</p>
+    <ul>
+        <li><strong>Recipient Address:</strong> {{ $shipment->recipient->address->state }}, {{ $shipment->recipient->address->city }}, {{ $shipment->recipient->address->street }}, {{ $shipment->recipient->address->details }}</li>
+        <li><strong>Tracking Token:</strong> {{ $shipment->tracking_token }}</li>
+    </ul>
+    <p style="text-align: center;">
+        <a href="http://127.0.0.1:8000/tracking" style="background-color: #007bff; color: #ffffff; padding: 10px 20px; text-decoration: none; display: inline-block;">Track Shipment</a>
+    </p>
+</div>
+
+<!-- Footer -->
+<div style="background-color: #f8f9fa; padding: 20px; text-align: center;">
+    <div style="margin: 0 auto; max-width: 600px; align-items: center; text-align: center; align-content: center;">
+        <p>{{ config('app.name') }}</p>
+        <p>123 St, Khartoum, Sudan</p>
+        <p>Phone: +123456789</p>
+        <p>Email: info@sudanbridge.com</p>
+    </div>
+
+    <!-- Social media icons with links -->
+    <a href="#"><img src="http://127.0.0.1:8000/svg/facebook.svg" alt="Facebook" style="margin-right: 10px;"></a>
+    <a href="#"><img src="http://127.0.0.1:8000/svg/X.svg" alt="Twitter" style="margin-right: 10px;"></a>
+    <a href="#"><img src="http://127.0.0.1:8000/svg/instagram.svg" alt="Instagram" style="margin-right: 10px;"></a>
+</div>
+</body>
+</html>
