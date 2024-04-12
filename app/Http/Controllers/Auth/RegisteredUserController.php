6ee8cab87905cda4c86991a7bd148e9457d96be5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-04-12 03:44:38 +0300

    Add logging functionality to store method in RegisteredUserController
    In this commit:
    - Added logging functionality to the store method
      in RegisteredUserController
    - Retrieve the created user with its related
      facility and role
    - Convert the user and its related data to JSON
      format
    - Log the new user data using the Log facade
    - Create a log entry for the user creation action
      using the createLog method

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Logs;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -48,6 +50,17 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+
+        // Retrieve the created user with its related facility and role
+        $createdUser = User::with('role', 'facility')->findOrFail($user->id);
+
+        // Convert the user and its related data to JSON
+        $oldData = $createdUser->toJson();
+        $newData = $createdUser->toJson();
+        Log::error($newData);
+
+        // Create log
+        $user->createLog($user->id, null, 'None', 'user-create', $oldData, $newData);
 
         return redirect(RouteServiceProvider::HOME);
     }
